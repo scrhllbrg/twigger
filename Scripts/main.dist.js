ea974b682781bb8b3ae182f891c1ec077b3a5bba
Oscar Hallberg <oscar.hallberg@happiness.se>
2020-09-28 17:19:17 +0200

    Add scafolding

---
+++ Scripts/main.dist.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function activate() {
+    console.log('Hello says Twigger.');
+}
+
+function deactivate() {
+    console.log('Goodbye from Twigger.');
+}
+
+exports.activate = activate;
+exports.deactivate = deactivate;
